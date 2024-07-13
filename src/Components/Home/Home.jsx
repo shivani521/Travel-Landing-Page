2998c805d600193f802f25f0983adee360aa3377
Shivani <shivanisharma.nitandhra@gmail.com>
2024-07-13 17:47:53 +0530

    updated the href

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,86 +1,84 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import "./home.css";
 import video from "../../Assets/video.mp4";
-import {GrLocation} from "react-icons/gr";
-import {HiFilter} from "react-icons/hi";
-import {FiFacebook} from "react-icons/fi";
-import {AiOutlineInstagram} from "react-icons/ai";
-import {BsListTask} from "react-icons/bs";
-import {SiTripadvisor} from "react-icons/si";
-import {TbApps} from "react-icons/tb";
+import { GrLocation } from "react-icons/gr";
+import { HiFilter } from "react-icons/hi";
+import { FiFacebook } from "react-icons/fi";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { BsListTask } from "react-icons/bs";
+import { SiTripadvisor } from "react-icons/si";
+import { TbApps } from "react-icons/tb";
 import Aos from 'aos';
 import 'aos/dist/aos.css';
-
 
 const Home = () => {
     //react hook to add scroll animation
     useEffect(() => {
-        Aos.init({duration: 2000})
-    }, {})
-    return(
+        Aos.init({ duration: 2000 });
+    }, []);
+
+    return (
         <section className="home">
             <div className="overlay"></div>
             <video src={video} muted autoPlay loop type="video/mp4"></video>
-        <div className="homeContent container">
-            <div className="textDiv">
-            <span data-aos="fade-up" className="smallText">
-                Our Packages
-            </span>
+            <div className="homeContent container">
+                <div className="textDiv">
+                    <span data-aos="fade-up" className="smallText">
+                        Our Packages
+                    </span>
 
-            <h1 data-aos="fade-up" className="homeTitle">
-                Search your Holiday
-            </h1>
-            </div>
-            <div  data-aos="fade-up" className="cardDiv grid">
-                <div className="destinationInput">
-                    <label htmlFor="city">
-                        Search your Destination:
-                    </label>
-                    <div className="input flex">
-                        <input type="text" placeholder="
-                        Enter name here..."/>
-                        <GrLocation className="icon"/>
-                    </div>
+                    <h1 data-aos="fade-up" className="homeTitle">
+                        Search your Holiday
+                    </h1>
                 </div>
+                <div data-aos="fade-up" className="cardDiv grid">
+                    <div className="destinationInput">
+                        <label htmlFor="city">
+                            Search your Destination:
+                        </label>
+                        <div className="input flex">
+                            <input type="text" placeholder="Enter name here..." id="city" />
+                            <GrLocation className="icon" />
+                        </div>
+                    </div>
 
-                <div className="dateInput">
-                    <label htmlFor="date">
-                        Select your date:
-                    </label>
-                    <div className="input flex">
-                        <input type="date"/>
+                    <div className="dateInput">
+                        <label htmlFor="date">
+                            Select your date:
+                        </label>
+                        <div className="input flex">
+                            <input type="date" id="date" />
+                        </div>
                     </div>
-                </div>
 
-                <div className="priceInput">
-                    <div className="label_total flex">
-                        <label htmlFor="price">Max Price:</label>
-                        <h3 className="total">$5000</h3>
+                    <div className="priceInput">
+                        <div className="label_total flex">
+                            <label htmlFor="price">Max Price:</label>
+                            <h3 className="total">$5000</h3>
+                        </div>
+                        <div className="input flex">
+                            <input type="range" max="5000" min="1000" id="price" />
+                        </div>
                     </div>
-                    <div className="input flex">
-                        <input type="range" max="5000" min="1000" />
+                    <div className="searchOptions flex">
+                        <HiFilter className="icon" />
+                        <span>MORE FILTERS</span>
                     </div>
                 </div>
-                <div className="searchOptions flex">
-                    <HiFilter className="icon"/>
-                    <span>MORE FILTERS</span>
+                <div data-aos="fade-up" className="homeFooterIcons flex">
+                    <div className="rightIcons">
+                        <FiFacebook className="icon" />
+                        <AiOutlineInstagram className="icon" />
+                        <SiTripadvisor className="icon" />
+                    </div>
+                    <div className="leftIcons">
+                        <BsListTask className="icon" />
+                        <TbApps className="icon" />
+                    </div>
                 </div>
             </div>
-            <div data-aos="fade-up" className="homeFooterIcons flex">
-                <div className="rightIcons">
-                    <FiFacebook className="icon"/>
-                    <AiOutlineInstagram className="icon"/>
-                    <SiTripadvisor className="icon"/>
-                </div>
-                <div className="leftIcons">
-                    <BsListTask className="icon"/>
-                    <TbApps className="icon"/>
-                </div>
-            </div>
-        </div>
-        
         </section>
-    )
+    );
 }
 
-export default Home
+export default Home;
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,7 +1,7 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import "./main.css";
-import {HiOutlineLocationMarker} from "react-icons/hi";
-import {HiOutlineClipboardCheck} from "react-icons/hi";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { HiOutlineClipboardCheck } from "react-icons/hi";
 
 import img from "../../Assets/img.jpg";
 import img1 from "../../Assets/img1.jpg";
@@ -14,95 +14,98 @@ import img7 from "../../Assets/img7.jpg";
 import img8 from "../../Assets/img8.jpg";
 import Aos from 'aos';
 import 'aos/dist/aos.css';
+
 const Data = [
     {
-        id:1,
+        id: 1,
         imgSrc: img,
-        destTitle : "Bora Bora",
+        destTitle: "Bora Bora",
         location: "New Zealand",
         grade: "CULTURAL RELAX",
         fees: "$700",
-        description: "The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for it's luxurious stays and adventurous activities."
+        description: "The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities."
     },
     {
-        id:2,
+        id: 2,
         imgSrc: img1,
-        destTitle : "Machu Picchu",
+        destTitle: "Machu Picchu",
         location: "Peru",
         grade: "CULTURAL RELAX",
         fees: "$600",
         description: "Explore the ancient Incan city nestled high in the Andes Mountains, offering breathtaking panoramic views of intricate stone architecture and mist-covered landscapes."
     },
     {
-        id:3,
+        id: 3,
         imgSrc: img2,
-        destTitle : "Great Barrier Reef",
+        destTitle: "Great Barrier Reef",
         location: "Australia",
         grade: "CULTURAL RELAX",
         fees: "$700",
         description: "Dive into the world's largest coral reef system, where vibrant marine life and mesmerizing coral formations create an underwater paradise teeming with color and wonder."
     },
     {
-        id:4,
+        id: 4,
         imgSrc: img3,
-        destTitle : "Cappadocia",
+        destTitle: "Cappadocia",
         location: "Turkey",
         grade: "CULTURAL RELAX",
         fees: "$800",
         description: "Uncover the enchanting Turkish region famous for its surreal landscapes of 'fairy chimneys' and cave dwellings, offering a unique hot air balloon perspective at sunrise."
     },
     {
-        id:5,
+        id: 5,
         imgSrc: img4,
-        destTitle : "Guanajuata",
+        destTitle: "Guanajuato",
         location: "Mexico",
         grade: "CULTURAL RELAX",
         fees: "$1100",
         description: "Wander through the vibrant streets of this Mexican town, known for its colorful buildings, subterranean roads, and rich cultural heritage that beckons to be explored."
     },
     {
-        id:6,
+        id: 6,
         imgSrc: img5,
-        destTitle : "Cinque Terre",
+        destTitle: "Cinque Terre",
         location: "Italy",
         grade: "CULTURAL RELAX",
         fees: "$840",
         description: "Traverse the picturesque Italian coastal villages perched along the Ligurian Sea, each characterized by its own charm, pastel-colored houses, and breathtaking cliffside views."
     },
     {
-        id:7,
+        id: 7,
         imgSrc: img6,
-        destTitle : "Angkor Wat",
+        destTitle: "Angkor Wat",
         location: "Cambodia",
         grade: "CULTURAL RELAX",
         fees: "$790",
         description: "Step into the awe-inspiring world of the largest religious monument, Cambodia's Angkor Wat, with its intricate temples and ancient ruins, reflecting a rich Khmer history."
     },
     {
-        id:8,
+        id: 8,
         imgSrc: img7,
-        destTitle : "Taj Mahal",
+        destTitle: "Taj Mahal",
         location: "India",
         grade: "CULTURAL RELAX",
         fees: "$900",
         description: "Behold the iconic ivory-white marble mausoleum in Agra, India, a symbol of eternal love and architectural brilliance, set against the backdrop of beautifully landscaped gardens."
     },
     {
-        id:9,
+        id: 9,
         imgSrc: img8,
-        destTitle : "Bali Island",
+        destTitle: "Bali Island",
         location: "Indonesia",
         grade: "CULTURAL RELAX",
         fees: "$500",
         description: "Immerse yourself in the Indonesian paradise of Bali, known for its lush rice terraces, serene beaches, vibrant cultural traditions, and spiritual experiences that soothe the soul."
     }
-]
+];
+
 const Main = () => {
     //react hook to add scroll animation
     useEffect(() => {
-        Aos.init({duration: 2000})
-    }, {})
-    return(
+        Aos.init({ duration: 2000 });
+    }, []);
+
+    return (
         <section className="main container section">
             <div className="secTitle">
                 <h3 data-aos="fade-right" className="title">
@@ -111,45 +114,43 @@ const Main = () => {
             </div>
 
             <div className="secContent grid">
-                {
-                    Data.map(({id, imgSrc, destTitle,
-                    location, grade, fees, description})=>{
-                        return(
-                            <div key={id} 
+                {Data.map(({ id, imgSrc, destTitle, location, grade, fees, description }) => {
+                    return (
+                        <div key={id}
                             data-aos="fade-up"
                             className="singleDestination">
-                                <div className="imageDiv">
-                                    <img src={imgSrc} alt={destTitle}/>
-                                </div>
-                                <div className="cardInfo">
-                                    <h4 className="destTitle">
-                                        {destTitle}
-                                    </h4>
-                                    <span className="continent flex">
-                                       <HiOutlineLocationMarker className="icon"/> 
-                                    <span className="name">{location}</span>  
-                                    </span>
-                                    <div className="fees flex">
-                                        <div className="grade">
-                                            <span>{grade}<small>+1</small></span></div>
-                                            <div className="price">
-                                                <h5>{fees}</h5></div>   
-                                        
-                                    </div>
-                                    <div className="desc">
-                                        <p>{description}</p>
-                                    </div>
-                                    <button className="btn flex">
-                                        DETAILS<HiOutlineClipboardCheck className="icon"/>
-                                    </button>
-                                </div>
+                            <div className="imageDiv">
+                                <img src={imgSrc} alt={destTitle} />
                             </div>
-                        )
-                    })
-                }
+                            <div className="cardInfo">
+                                <h4 className="destTitle">
+                                    {destTitle}
+                                </h4>
+                                <span className="continent flex">
+                                    <HiOutlineLocationMarker className="icon" />
+                                    <span className="name">{location}</span>
+                                </span>
+                                <div className="fees flex">
+                                    <div className="grade">
+                                        <span>{grade}<small>+1</small></span>
+                                    </div>
+                                    <div className="price">
+                                        <h5>{fees}</h5>
+                                    </div>
+                                </div>
+                                <div className="desc">
+                                    <p>{description}</p>
+                                </div>
+                                <button className="btn flex">
+                                    DETAILS<HiOutlineClipboardCheck className="icon" />
+                                </button>
+                            </div>
+                        </div>
+                    );
+                })}
             </div>
         </section>
-    )
+    );
 }
 
-export default Main
+export default Main;
